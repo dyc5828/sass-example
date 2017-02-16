7a15eedab963696f329ebb28c482c89aebe91611
Dan Chen <dyc5828@gmail.com>
2017-02-15 16:16:11 -0800

    added gulp-sass in comments

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,16 @@ const elixir = require('laravel-elixir')
 elixir(mix => {
 	mix.sass('./src/main.scss', 'dist/main.min.css')
 })
+
+// const gulp = require('gulp')
+// const sass = require('gulp-sass')
+
+// gulp.task('default', () => {
+// 	return gulp.src('./src/main.scss')
+// 		.pipe(sass().on('error', sass.logError))
+// 		.pipe(gulp.dest('./dist'))
+// })
+// 
+// gulp.task('watch', () => {
+// 	return gulp.watch('./src', ['default'])
+// })
